d0595db57a1af4cec9481b3b6e9f5a65a46ee8d0
Stefan Lyew <stefan.lyew@gmail.com>
2013-08-27 22:01:30 -0400

    Initialized Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtsiran::Application.config.secret_token = '1a84694b0c175a5838bbf4efba543f4cccb7430678291dad3c8a7b6ecbef1c5ff99d2e4cd5b05f579076cf50ce0f9c3964b69e9adcceb07d8e907d37850acf5c'
