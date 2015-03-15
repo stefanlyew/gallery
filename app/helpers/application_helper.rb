0eb3fce0aa1decdce7912f0ef74ec251b9a70813
Stefan Lyew <stefan.lyew@gmail.com>
2015-03-14 19:11:43 -0500

    fix padding for navs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-	def sortable(column, title = nil)
-	  title ||= column.titleize
-	  css_class = column == sort_column ? "current #{sort_direction}" : nil
-	  direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-	  link_to title, {:sort => column, :direction => direction}, {:class => css_class}
-	end
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
 
-	def big_tag_list
-		Tag.all.map(&:name)
-	end
+  def big_tag_list
+    Tag.all.map(&:name)
+  end
 
   def featured_galleries
     Gallery.featured.all
@@ -18,12 +18,14 @@ module ApplicationHelper
     Nav.featured.all
   end
 
-	def nav_entry(body, path)
-		content_tag(:li, link_to(body, path), class: ('active' if current_page?(path)))
-	end
+  def nav_entry(body, path)
+    content_tag(:li, link_to(body, path), class: ('active' if current_page?(path)))
+  end
 
 
-	def profile_id
-		Profile.last.id
-	end
+  def profile_id
+    if Profile.last
+      return Profile.last.id
+    end
+  end
 end
